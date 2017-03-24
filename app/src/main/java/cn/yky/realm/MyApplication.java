27a6ecab39a12fc18324d1d488769894d5ae5175
于阔源 <yukuoyuan@hotmail.com>
2017-03-24 14:05:52 +0800

    提交了可以查看Realm数据的依赖

--- app/src/main/java/cn/yky/realm/MyApplication.java
+++ app/src/main/java/cn/yky/realm/MyApplication.java
@@ -2,6 +2,11 @@ package cn.yky.realm;
 
 import android.app.Application;
 
+import com.facebook.stetho.Stetho;
+import com.uphyca.stetho_realm.RealmInspectorModulesProvider;
+
+import java.util.regex.Pattern;
+
 import io.realm.Realm;
 
 /**
@@ -13,5 +18,10 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Realm.init(this);
+        Stetho.initialize(
+                Stetho.newInitializerBuilder(this)
+                        .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                        .enableWebKitInspector(RealmInspectorModulesProvider.builder(this).build())
+                        .build());
     }
 }
